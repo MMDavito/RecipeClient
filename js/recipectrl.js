81b5702daabe3fde91aab3e7befe0a466083e1e3
MMDavito <davidking9753@gmail.com>
2017-01-17 11:22:50 +0100

    INIT commit recipeClient

--- js/recipectrl.js
+++ js/recipectrl.js
@@ -4,21 +4,38 @@ module.config(function ($urlRouterProvider, $stateProvider) {
     $stateProvider.state("home", {
         url: "/",
         templateUrl: "templates/home.html",
-        controller: "homeCtrl"}).state("login", {
+        controller: "homeCtrl"}).state("recipe", {
+        url: "/{id}",
+        templateUrl: "templates/recipe.html",
+        controller: "recipeCtrl"
+    }).state("login", {
         url: "/login",
         templateUrl: "templates/login.html",
         controller: "loginCtrl"
     });
 });
-module.controller("homeCtrl", function ($scope, $rootScope, sportService) {
+module.controller("homeCtrl", function ($scope, $rootScope, recipeService) {
     var promise = recipeService.getRecipes();
     promise.then(function (data) {
-        $scope.table = data;
+        $scope.recipes = data.data;
+        console.log(data.data);
     });
+     $scope.fillRecipe = recipeService.fillRecipe();
 });
 module.controller("loginCtrl", function ($scope, $rootScope, recipeService) {
     $scope.loggIn = function () {
         console.log("du komer till login");
         recipeService.loggIn($scope.username, $scope.password);
     };
+});
+module.controller("recipeCtrl", function ($scope, recipeService) {
+        var promise = recipeService.getRecipeIngs();
+    promise.then(function (data) {
+    var promise = recipeService.getRecipes();
+    promise.then(function (data) {
+        $scope.recipe = data.data;
+        console.log(data.data);
+
+    });
+});
 });
--- js/recipeservice.js
+++ js/recipeservice.js
@@ -1,4 +1,12 @@
 module.service("recipeService", function ($q, $http, $rootScope) {
+    this.getRecipes = function () {
+        var deffer = $q.defer();
+        var url = "http://localhost:8080/RecipeServer.0.1/webresources/recipes";
+        $http.get(url).then(function (data) {
+            deffer.resolve(data);
+        });
+        return deffer.promise;
+    };
     this.loggIn = function (username, password) {
 
         var url = "http://localhost:8080/RecipeServer.0.1/webresources/login";
@@ -11,10 +19,15 @@ module.service("recipeService", function ($q, $http, $rootScope) {
             method: "POST",
             headers: {'Authorization': auth}
         }).then(function (data, status) {
+            console.log("data "+data);
+            console.log("status "+status);
             console.log("loggedin");
             $rootScope.isLoggedIn = true;
             $rootScope.user = username;
             $rootScope.pass = password;
         });
     };
+    this.fillRecipe = function (id) {
+        console.log(id);    
+};
 });
